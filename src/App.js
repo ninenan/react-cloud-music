918dcd32f9ea11ba27bd281972a0d3f9f94a996b
wuqiangnan <2441277210@qq.com>
2022-05-31 23:15:49 +0800

    feat: home

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import './App.css';
 import { GlobalStyle } from './style/reset'
 import { IconStyle } from './style/iconfont'
-import { HashRouter } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 
-import MyNavLink from './components/MyNavLink'
 import Home from './pages/Home';
-import Recommend from './pages/Recommend'
-import Singers from './pages/Singers';
-import Rank from './pages/Rank';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
       <HashRouter>
-        {/* {renderRoutes(routes)} */}
-        <GlobalStyle></GlobalStyle>
-        <header className="App-header">
-          <IconStyle></IconStyle>
-          <i className="iconfont">&#xe62b;</i>
-
-          <MyNavLink key="recommend" to={'/recommend'}>recommand</MyNavLink>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/recommend' element={<Recommend />} />
-            <Route path='/singers' element={<Singers />} />
-            <Route path='/rank' element={<Rank />} />
-          </Routes>
-        </header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </HashRouter>
     </div>
   );
 }
 
-export default App;
+export default App
+
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,43 @@
-export default function Home() {
-    return (
-        <> home page</>
-    )
+import React from 'react';
+import { NavLink, Routes, Route, HashRouter } from 'react-router-dom'
+import { Top, Tab, TabItem } from './style'
+
+import Recommend from '../Recommend'
+import Singers from '../Singers';
+import Rank from '../Rank';
+
+function Home() {
+  return (
+    <>
+      <Top>
+        <span className='iconfont menu'>&#xe65c;</span>
+        <span className='title'>cloud-music</span>
+        <span className='iconfont search'>&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend">
+          <TabItem>
+            <span>推荐</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/singers">
+          <TabItem>
+            <span>歌手</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/rank">
+          <TabItem>
+            <span>排行榜</span>
+          </TabItem>
+        </NavLink>
+      </Tab>
+      <Routes>
+        <Route path="/recommend" element={<Recommend />} />
+        <Route path="/rank" element={<Rank />} />
+        <Route path="/singers" element={<Singers />} />
+      </Routes>
+    </>
+  )
 }
+
+export default React.memo(Home)
